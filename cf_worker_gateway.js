ec76497498d89561c40a5835494f8e3cf7931031
hfl618 <13102109882@163.com>
2026-02-17 21:22:54 +0800

    feat: complete global SEO system, robots.txt, sitemap, and baidu push integration

--- cf_worker_gateway.js
+++ cf_worker_gateway.js
@@ -544,6 +544,17 @@ function renderIndex(user) {
 
     document.getElementById('globalSearch').addEventListener('input', apply);
     setLanguage(currentLang);
+  </script>
+  <script>
+    // 🚀 百度自动推送脚本
+    (function(){
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') { bp.src = 'https://zz.bdstatic.com/linksubmit/push.js'; }
+        else { bp.src = 'http://push.zhanzhang.baidu.com/push.js'; }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    })();
   </script>` + BUG_WIDGET + `</body>
 </html>`;
 }
